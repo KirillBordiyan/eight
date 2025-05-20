c1a92abab77a708ad8b32c76bcf669fbf1003091
Kirill Bordiyan <kirillbordiyan@gmail.com>
2025-05-20 16:11:25 +0300

    exception handler upd

--- disable-user-module/src/main/java/com/social/exceptions/GlobalExceptionHandler.java
+++ disable-user-module/src/main/java/com/social/exceptions/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalExecptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
