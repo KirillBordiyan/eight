1e3dc5787f0981d0a6d51fe0e14c97b4be7dac5a
Kirill Bordiyan <kirillbordiyan@gmail.com>
2025-05-22 03:35:44 +0300

    try

--- core-module/src/main/java/com/social/Main.java
+++ core-module/src/main/java/com/social/Main.java
@@ -3,7 +3,5 @@ package com.social;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Unt u = new Unt();
-        u.some();
     }
 }
